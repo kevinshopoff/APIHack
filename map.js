c3e3b8419eeb127e1e77ed94be3e0f51ff3ba876
Kevin Shopoff <kshopoff@gmail.com>
2013-11-11 15:58:52 -0600

    Updates to add directions
    2nd input box, added directions div

--- map.js
+++ map.js
@@ -5,7 +5,7 @@ var startLat;
 var startLong;
 var endLat;
 var endLong;
-var json = null;
+var result = null;
 
 function initialize() {
 	var mapOptions = {
@@ -45,6 +45,7 @@ function loadScript() {
 
 
 function getDirections(start, finish) {
+	var directionsService = new google.maps.DirectionsService();
 	console.log('Get Directions from: ' + start + ' to: ' + finish);
 	/*var script = document.createElement('script');
 	script.type = 'text/javascript';
@@ -52,26 +53,48 @@ function getDirections(start, finish) {
 		'callback=blah';
 	console.log(script.src);
 	document.body.appendChild(script);*/
-	$.ajax({
+	/*$.ajax({
 		'async': false,
 		'global': false,
 		'url' : 'http://maps.googleapis.com/maps/api/directions/json?origin=' + start + '&destination=' + finish + '&sensor=false&' + 
 		'callback=blah',
-		'dataType': "json",
+		'dataType': "jsonp",
 		'success': function(data) {
 			json = data;
+		},
+		'error': function(data, status) {
+			console.log('oops! an error.');
 		}
 	});
 	
-	console.log('json is: ' + json);
+	console.log('json is: ' + json);*/
 	
+	var request = {
+		origin:start,
+		destination:finish,
+		travelMode: google.maps.TravelMode.DRIVING
+	};
+	directionsService.route(request, function(result, status) {
+		if (status == google.maps.DirectionsStatus.OK) {
+			console.log('result = ' + result.routes[0].legs[0].steps[3]./*html_instructions*//*duration.text*/html_instructions);
+			console.log('num steps = ' + result.routes[0].legs[0].steps.length);
+			for (i = 0; i<result.routes[0].legs[0].steps.length; i++) {
+				/*$('#directions').append('<p>' + result.routes[0].legs[0].steps[i].html_instructions + '</p>');*/
+				$('#directions').append('<p>' + result.routes[0].legs[0].steps[i].distance.text + '</p>');
+				$('#directions').append('<p>' + result.routes[0].legs[0].steps[i].duration.text + '</p>');
+				$('#directions').append('<p>' + result.routes[0].legs[0].steps[i].maneuver + '</p>');
+				$('#directions').append('<p>' + result.routes[0].legs[0].steps[i].travel_mode + '</p><br>')
+				console.log('i = ' + i + ": " + result.routes[0].legs[0].steps[i].distance.text);
+			};
+		}
+	});
 	
 	console.log('end of Get Directions');
 	
 	
 	
 	/*there needs to be some kind of return value set*/
-	return json;
+	return result;
 	    /*$.ajax({ 
         type: 'GET',
         url: 'http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=false' ,
@@ -88,7 +111,7 @@ function blah(data) {
 };
 
 
-	function plotAddress(mySearch) {
+	function plotAddress(startPoint, mySearch) {
 		var address = mySearch;
 		geocoder.geocode( {'address': address}, function(results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
@@ -115,7 +138,8 @@ function blah(data) {
 $(document).ready(function() {	
 	/*when the button is clicked*/
 	$("#btn").on("click", function() {
-		var currentSearch = $('input[name="searchString"]').val();
+		var currentSearch = $('input[name="searchTo"]').val();
+		var startingPoint = $('input[name="searchFrom"]').val();
 		console.log("You want to search for " + currentSearch);
 		plotAddress(currentSearch)
 		return false;
@@ -125,9 +149,10 @@ $(document).ready(function() {
 	/*when Enter is pressed*/
 	$('body').on("keypress", function(event) {
 		if (event.which === 13) {
-			var currentSearch = $('input[name="searchString"]').val();
+			var currentSearch = $('input[name="searchTo"]').val();
+			var startingPoint = $('input[name="searchFrom"]').val();
 			console.log("You want to search for " + currentSearch);
-			plotAddress(currentSearch);
+			plotAddress(startingPoint, currentSearch);
 			return false;
 		}
 	});
