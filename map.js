1723eaca4d2d22a33f11648cdeedca05c86e5afa
Kevin Shopoff <kshopoff@gmail.com>
2013-11-13 22:58:37 -0600

    Finished for now
    Formatted directions to prevent them from moving below map, added
    polyline (route), starting point and ending point to the map, auto
    zoomed map to contain both starting and ending

--- map.js
+++ map.js
@@ -1,11 +1,16 @@
 var google = google || {};
-var geocoder;
 var map;
+var startMarker;
+var endMarker;
 var startLat;
 var startLong;
 var endLat;
 var endLong;
 var result = null;
+var myRouteCoordinates = []; /*added for routing*/
+var myRoute;
+var LatLngList = [];
+
 
 function initialize() {
 	var mapOptions = {
@@ -15,10 +20,7 @@ function initialize() {
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  
-	geocoder = new google.maps.Geocoder();
- 
-	console.log('geocoder: ', geocoder);
+  	
 }
 
 function loadScript() {
@@ -30,6 +32,8 @@ function loadScript() {
 }
 
 function getDirections(start, finish) {
+	removeLine();  /*removes any existing polylines and markers*/
+	
 	var directionsService = new google.maps.DirectionsService();
 	console.log('Get Directions from: ' + start + ' to: ' + finish);
 	
@@ -40,9 +44,41 @@ function getDirections(start, finish) {
 	};
 	directionsService.route(request, function(result, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
-			console.log('result = ' + result.routes[0].legs[0].steps[3]./*html_instructions*//*duration.text*/instructions);
 			console.log('num_steps = ' + result.routes[0].legs[0].steps.length);
 			$("#directions").empty();
+
+			/*added for routing*/
+			LatLngList = []; //holds the map boundaries
+			myRouteCoordinates = []; //list of lat_lngs for the polyline
+			
+			var startLatLng = new google.maps.LatLng(result.routes[0].legs[0].start_location.ob, result.routes[0].legs[0].start_location.pb);
+			var endLatLng = new google.maps.LatLng(result.routes[0].legs[0].end_location.ob, result.routes[0].legs[0].end_location.pb);
+			
+			LatLngList.push(startLatLng); //add the start point to the map boundaries
+			LatLngList.push(endLatLng); //add the end point to the map boundaries
+			
+			var bounds = new google.maps.LatLngBounds();
+			
+			for (i = 0; i < LatLngList.length; i++) {
+				bounds.extend (LatLngList[i]);
+			};
+			
+			map.fitBounds(bounds);  //zoom and position the map so the end points are displayed
+						
+			myRouteCoordinates.push(startLatLng);  //add start point to the list of route coordinates for the polyline
+						
+			startMarker = new google.maps.Marker({ //add the marker for the starting point
+				position: startLatLng,
+				map: map,
+				icon: "http://maps.google.com/mapfiles/ms/micons/green-dot.png"
+			});
+			
+			endMarker = new google.maps.Marker({ //add the marker for the ending point
+				map: map,
+				position: endLatLng,
+				icon: "http://maps.google.com/mapfiles/ms/micons/red-dot.png"
+			});
+			
 			for (i = 0; i<result.routes[0].legs[0].steps.length; i++) {
 				var ptag = null;
 				var stepNum = i + 1;
@@ -56,10 +92,29 @@ function getDirections(start, finish) {
 					ptag = '<p>'
 				};
 				
-				$('#directions').append(ptag + stepNum + '. ' + instr + ' for ' + dist + ' (' + dur + ').</p>');
+				//add the turn by turn directions to the UI
+				$('#directions').append(ptag + stepNum + '. ' + instr + ' for ' + dist + ' (' + dur + ').</p>'); 
 				
 				console.log('result = ' + result.routes[0].legs[0].steps[i].instructions);
+				
+				//add all the lat_lngs to the polyline array
+				for (j=0; j<result.routes[0].legs[0].steps[i].lat_lngs.length; j++) {
+					myRouteCoordinates.push(new google.maps.LatLng(result.routes[0].legs[0].steps[i].lat_lngs[j].ob, result.routes[0].legs[0].steps[i].lat_lngs[j].pb));
+				};
 			};
+			
+			//create the polyline
+			myRoute = new google.maps.Polyline({
+				path: myRouteCoordinates,
+				geodesic: true,
+				strokeColor: '#FF0000',
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+			});
+			
+			//add the polyline to the map
+			myRoute.setMap(map);
+			
 		}
 	});
 	
@@ -78,31 +133,24 @@ function getDirections(start, finish) {
         }
     });*/
 }
-
-	function plotAddress(startLoc, endLoc) {
-		var startAddress = startLoc;
-		var endAddress = endLoc;
-		geocoder.geocode( {'address': endAddress}, function(results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
-				startLat = results[0].geometry.location.lb;
-				startLong = results[0].geometry.location.mb;
-				var marker = new google.maps.Marker({
-					map: map,
-					position: results[0].geometry.location
-				});
-				var hereToThere = getDirections(startAddress, endAddress);
-				console.log('Got back to plotAddress');
-				console.log('hereToThere is: ' + hereToThere);
-				/*var hereToThere = 'http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=false&callback=blah'
-				$.getJSON(hereToThere, function(data) {
-					console.log(data);
-				});*/
-			} else {
-				alert('Geocode was not successful for the following reason: ' + status);
-			}
-		});
-	}
+	
+function removeLine() {
+	if (myRoute) {
+		myRoute.setMap(null);
+		myRoute = null;
+	};
+	if (startMarker) {
+		startMarker.setMap(null);
+		startMarker = null;
+	};
+	if (endMarker) {
+		console.log("endMarker exists");
+		endMarker.setMap(null);
+		endMarker = null;
+	} else {
+		console.log("endMarker does not exist");
+	};
+}
 	
 $(document).ready(function() {	
 	/*when the button is clicked*/
@@ -110,18 +158,17 @@ $(document).ready(function() {
 			var endPoint = $('input[name="searchTo"]').val();
 			var startingPoint = $('input[name="searchFrom"]').val();
 			console.log("You want to go from " + startingPoint + " to " + endPoint);
-			plotAddress(startingPoint, endPoint);
+			getDirections(startingPoint, endPoint);
 		return false;
 	});
 
-	/*this still needs to be updated for the map hack*/
 	/*when Enter is pressed*/
 	$('body').on("keypress", function(event) {
 		if (event.which === 13) {
 			var endPoint = $('input[name="searchTo"]').val();
 			var startingPoint = $('input[name="searchFrom"]').val();
 			console.log("You want to go from " + startingPoint + " to " + endPoint);
-			plotAddress(startingPoint, endPoint);
+			getDirections(startingPoint, endPoint);
 			return false;
 		}
 	});
